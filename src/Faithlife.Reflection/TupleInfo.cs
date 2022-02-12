f296382364513c1f3f7e51212b90aea0de67d3c5
Ed Ball <ed.ball@faithlife.com>
2022-02-12 15:08:13 -0800

    Remove unused usings.

--- src/Faithlife.Reflection/TupleInfo.cs
+++ src/Faithlife.Reflection/TupleInfo.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
 
--- tools/Benchmarks/CreateNewBenchmark.cs
+++ tools/Benchmarks/CreateNewBenchmark.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Reflection;
 using BenchmarkDotNet.Attributes;
 using Faithlife.Reflection;
